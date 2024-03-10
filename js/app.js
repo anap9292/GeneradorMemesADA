eb55bbd850dd5851004e3b1190fb0c371c085984
anap9292 <anahiandrea.p@gmail.com>
2024-03-10 17:26:31 -0300

    agrego ventana modal y funcionamiento

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 const btnNoche = document.getElementById('btn-noche');
+const btnTxt = document.getElementById('btn-txt');
+const btnCerrar = document.getElementById('cerrar');//cerrar
 const contenedor = document.getElementById('contenedor');
 const txtModo = document.getElementById('txt-modo'); //txt-modo
 const header = document.getElementById('header');
+//const txtTop = document.getElementById('txt-top'); 
+const txtInferior = document.getElementById('txt-inferior');
+const modal = document.getElementById('modal');
+
 
 btnNoche.addEventListener('click', function () {
 
@@ -11,6 +17,14 @@ btnNoche.addEventListener('click', function () {
     txtModo.textContent = modo;
 
 
+});
+
+btnTxt.addEventListener('click', function () {
+    modal.style.display = 'block';
+});
+
+btnCerrar.addEventListener('click', function () {
+    modal.style.display = 'none';
 });
 
 console.log('hola en la consola')
